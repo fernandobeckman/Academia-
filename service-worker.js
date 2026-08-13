e8a48aaec52694c21c458e7686957ffccf9a27bc
fernandobeckman <fernandobeckman2007@gmail.com>
2026-08-13 17:50:35 -0300

    upload: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bulk-log-v3";
+const CACHE_NAME = "bulk-log-v4";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./musc-map.jpg"];
 
 self.addEventListener("install", (event) => {
