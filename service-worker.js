b0a17eae41cafc1fbac860288bcecdb003ec47ff
fernandobeckman <fernandobeckman2007@gmail.com>
2026-08-14 23:41:35 -0300

    fix: bump cache legende-v7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "legende-v7";
+const CACHE_NAME = "legende-v8";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./musc-map.jpg"];
 
 self.addEventListener("install", (event) => {
